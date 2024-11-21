c623878c01188c55ce4bfefd018b542ed5567904
SakibSibly <sibly@duck.com>
2024-11-21 12:16:14 +0600

    Serializers for the Faculty and the Department added

---
+++ backend/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Department, Faculty
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+
+class FacultySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Faculty
+        fields = '__all__'
