962dccb7b42a4613a6d4f5653b1cf0e1220b3851
SakibSibly <sibly@duck.com>
2024-12-08 09:20:24 +0600

    Models for blog, comment, event and notices added with other classes being commented out for further improvemoent

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,42 @@
+# from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, email, username, password=None, **extra_fields):
+#         if not email:
+#             raise ValueError("The Email field must be set")
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, username=username, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(self, email, username, password=None, **extra_fields):
+#         extra_fields.setdefault('is_staff', True)
+#         extra_fields.setdefault('is_superuser', True)
+
+#         return self.create_user(email, username, password, **extra_fields)
+
+
+# class CustomUser(AbstractBaseUser, PermissionsMixin):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     username = models.CharField(max_length=50, unique=True)
+#     email = models.EmailField(unique=True)
+#     country = models.CharField(max_length=50)
+#     city = models.CharField(null=True, max_length=50)
+#     is_active = models.BooleanField(default=True)
+#     is_staff = models.BooleanField(default=False)  # Regular user, not staff by default
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     objects = CustomUserManager()
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['username']
+
+#     def __str__(self):
+#         return self.username
 
 
 class Department(models.Model):
@@ -10,6 +48,7 @@ class Department(models.Model):
     def __str__(self):
         return self.full_name
 
+
 class Faculty(models.Model):
     name = models.CharField(max_length=100)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
@@ -20,3 +59,76 @@ class Faculty(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    # author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    # user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    venue = models.CharField(max_length=200)
+    # image = models.ImageField(upload_to='event_images/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Notice(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+# class Gallery(models.Model):
+#     title = models.CharField(max_length=200)
+#     description = models.TextField()
+#     # image = models.ImageField(upload_to='gallery_images/')
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return self.title
+
+
+# class Feedback(models.Model):
+#     # name = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+#     message = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return self.name
+
+
+# class About(models.Model):
+#     title = models.CharField(max_length=200)
+#     description = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return self.title
