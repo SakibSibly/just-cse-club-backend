11ce5dea87ad9ef6be500f6358d1eb1f48a2f46a
SakibSibly <sibly@duck.com>
2024-12-08 09:17:00 +0600

    URLs for blogs, comments, events, notices added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,4 +7,13 @@ urlpatterns = [
     path('departments/<int:serial_number>/', views.DepartmentDetailView.as_view(), name='department-detail'),
     path('faculties/', views.FacultyListView.as_view(), name='faculty-list'),
     path('faculties/<int:faculty_id>/', views.FacultyDetailView.as_view(), name='faculty-detail'),
+    path('blogs/', views.BlogListView.as_view(), name='blog-list'),
+    path('blogs/<int:blog_id>/', views.BlogDetailView.as_view(), name='blog-detail'),
+    path('comments/', views.CommentListView.as_view(), name='comment-list'),
+    path('comments/<int:comment_id>/', views.CommentDetailView.as_view(), name='comment-detail'),
+    path('events/', views.EventListView.as_view(), name='event-list'),
+    path('events/<int:event_id>/', views.EventDetailView.as_view(), name='event-detail'),
+    path('notices/', views.NoticeListView.as_view(), name='notice-list'),
+    path('notices/<int:notice_id>/', views.NoticeDetailView.as_view(), name='notice-detail'),
+    
 ]
