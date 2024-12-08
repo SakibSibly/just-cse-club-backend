c22aeb058ecc0aa2c997695f3d4510600b7f421b
SakibSibly <sibly@duck.com>
2024-12-08 09:48:56 +0600

    Object representation for blog and comment in admin pannel changed

--- backend/api/models.py
+++ backend/api/models.py
@@ -70,6 +70,9 @@ class Blog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
@@ -77,6 +80,9 @@ class Comment(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text[:50] + '...'
 
 
 class Event(models.Model):
