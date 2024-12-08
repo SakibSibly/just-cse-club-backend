bf0bd995a83de32487ce1de042003955add7d035
SakibSibly <sibly@duck.com>
2024-12-08 09:18:49 +0600

    Serializers for blogs, comments, events and notices added

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Department, Faculty
+from .models import Department, Faculty, Blog, Comment, Event, Notice
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
@@ -12,3 +12,28 @@ class FacultySerializer(serializers.ModelSerializer):
     class Meta:
         model = Faculty
         fields = '__all__'
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+
+class NoticeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notice
+        fields = '__all__'
+
