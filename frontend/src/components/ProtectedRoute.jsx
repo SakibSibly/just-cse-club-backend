ca4c17820b0fd4bac2b1da6d4685e711b3fb4949
SakibSibly <sibly@duck.com>
2024-12-31 21:52:03 +0600

    Components are made separate in components folder

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import {Navigate} from 'react-router-dom';
+import {useState} from 'react';
+
+const ProtectedRoute = ({ children }) => {
+    const [isAuthorized, setIsAuthorized] = useState(null);
+
+    
+
+}
