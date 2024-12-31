1109e915f2680b02e43f4fc4727bce8c3cda068f
SakibSibly <sibly@duck.com>
2024-12-31 23:01:13 +0600

    Sample frontend structure with basics created

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from './constants';
 
 
 const api = axios.create({
@@ -7,6 +8,15 @@ const api = axios.create({
 
 api.interceptors.request.use(
     (config) => {
-        const token = 
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
 )
+
+export default api;
--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import {Navigate} from 'react-router-dom';
-import {useState} from 'react';
-
-const ProtectedRoute = ({ children }) => {
-    const [isAuthorized, setIsAuthorized] = useState(null);
-
-    
-
-}
