3c554397fab6b552914049a1927e024af8697dcd
SakibSibly <sibly@duck.com>
2025-01-01 16:11:22 +0600

    Route to pages are wrapped inside the Main Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import react from 'react'
+import "./assets/css/App.css"
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import MainComponent from './shared/MainComponent/MainComponent'
 import Home from "./pages/Home/Home"
 import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register"
 import Events from './pages/Events/Events'
+import Notices from './pages/Notices/Notices'
 import About from './pages/About/About'
 import Treasury from './pages/Treasury/Treasury'
 import NotFound from "./pages/NotFound/NotFound"
@@ -25,15 +27,17 @@ function App() {
     <>
      <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="/" element={<MainComponent children={<Home />} />} />
+        <Route path="/home" element={<MainComponent children={<Home />} />} />
+        <Route path="/login" element={<MainComponent children={<Login />} />} />
+        <Route path="/register" element={<MainComponent children={<RegisterAndLogout />} />} />
         <Route path="/logout" element={<Logout />} />
-        <Route path='/events' element={<Events />} />
-        <Route path='/about' element={<About />} />
+        <Route path='/events' element={<MainComponent children={<Events />} />} />
+        <Route path='/notices' element={<MainComponent children={<Notices />} />} />
+        <Route path='/about' element={<MainComponent children={<About />} />} />
         <Route path="/treasury" element={
           <ProtectedRoute>
-            <Treasury />
+            <MainComponent children={<Treasury />} />
           </ProtectedRoute>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
